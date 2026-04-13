e463b387fc767cdf32f804419b6bfd2c215d22c1
hogolezcano92-pixel <hogolezcano92@gmail.com>
2026-04-13 06:09:55 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -144,10 +144,14 @@ function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
+    if ('scrollRestoration' in window.history) {
+      window.history.scrollRestoration = 'manual';
+    }
+
     window.scrollTo({
       top: 0,
       left: 0,
-      behavior: 'smooth'
+      behavior: 'auto'
     });
   }, [pathname]);
 
