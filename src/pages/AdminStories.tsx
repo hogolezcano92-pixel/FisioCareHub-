840af8d9225df15d9ba5434d6d3502306ac7b6f9
FisioCareHub.company <hogolezcano92@gmail.com>
2026-05-28 18:33:18 -0300

    Add files via upload

---
+++ src/pages/AdminStories.tsx
@@ -0,0 +1,11 @@
+import AdminStoriesModeration from '../components/FisioStories/AdminStoriesModeration';
+
+export default function AdminStories() {
+  return (
+    <div className="min-h-screen bg-slate-950 px-4 py-10 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl">
+        <AdminStoriesModeration />
+      </div>
+    </div>
+  );
+}
