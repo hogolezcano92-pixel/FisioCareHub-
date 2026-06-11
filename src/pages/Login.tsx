8313f874b8c1ae7d98ebe54a9d9c658807dd6cdd
FisioCareHub.company <hogolezcano92@gmail.com>
2026-06-11 16:53:31 -0300

    Add files via upload

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -56,7 +56,7 @@ export default function Login() {
 
   const keepPremiumSplashVisible = () =>
     new Promise<void>((resolve) => {
-      window.setTimeout(resolve, 5000);
+      window.setTimeout(resolve, 7600);
     });
 
   useEffect(() => {
