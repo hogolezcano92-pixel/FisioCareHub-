77562821092cb941c827d57a23aa12c0a220fd34
FisioCareHub.company <hogolezcano92@gmail.com>
2026-06-15 15:18:39 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -752,7 +752,7 @@ function AppContent() {
               "flex-1 w-full",
               !isLoginPage && !showSidebar && !isAdminPage && !isWaitingPage && location.pathname !== '/chat' && "py-4 md:py-8",
               showSidebar && location.pathname !== '/chat' && "p-4 md:p-8 lg:p-10",
-              showMobileBottomNavigation && "bottom-nav-page"
+              showMobileBottomNavigation && "pb-28 md:pb-0"
             )}>
               <Suspense fallback={<PageLoader />}>
                 <Routes>
